613037a2d91d7663f60cb2213c9ef2fa89667650
DDumas551 <david.j.dumas@gmail.com>
2019-02-18 20:47:33 -0800

    Start of code

---
+++ javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(()=> {
+    setTimeout(() => {
+        $(".nameCol").addClass("nameColOnLoad");
+    }, 400 );
+    setTimeout(() => {
+        $(".intro1").addClass("intro11");
+    }, 1100 );
+    setTimeout(() => {
+        $(".intro2").addClass("intro22");
+    }, 1300 );
+});
+
+
