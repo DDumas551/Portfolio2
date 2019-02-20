f4a926032260caef00b0ebd2065d7ea48a30780a
DDumas551 <david.j.dumas@gmail.com>
2019-02-19 22:17:12 -0800

    Finishes front page and transitions

--- javascript.js
+++ javascript.js
@@ -8,6 +8,18 @@ $(document).ready(()=> {
     setTimeout(() => {
         $(".intro2").addClass("intro22");
     }, 1300 );
+    setTimeout(() => {
+        $(".hr1").addClass("hr2");
+    }, 1500 );
+    setTimeout(() => {
+        $(".knowledge1").addClass("knowledgeOnLoad1");
+    }, 1700 );
+    setTimeout(() => {
+        $(".knowledge2").addClass("knowledgeOnLoad2");
+    }, 1900 );
+    setTimeout(() => {
+        $(".bottomRow").addClass("bottomRowOnLoad");
+    }, 2100 );
 });
 
 
