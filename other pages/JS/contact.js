b3418a5575a18ac726c121d8f30c265738b78cb3
DDumas551 <david.j.dumas@gmail.com>
2019-03-13 15:48:07 -0700

    Finishes contact page for now

--- other pages/JS/contact.js
+++ other pages/JS/contact.js
@@ -2,18 +2,26 @@ $(document).ready(()=> {
     setTimeout(() => {
         $(".nameCol").addClass("nameColOnLoad");
     }, 400 );
+
     setTimeout(() => {
         $(".hr1").addClass("hr2");
     }, 500 );
-    setTimeout(() => {
-        $(".pdfImg").addClass("pdfImgOnLoad");
-    }, 1000 );
-    setTimeout(() => {
-        $(".bottomRow").addClass("bottomRowOnLoad");
-    }, 1250 );
+
     setTimeout(() => {
         $(".contactInvitation").addClass("contactInvitationOnLoad");
-    }, 2200);
+    }, 1000);
+
+    setTimeout(() => {
+        $(".contactIcon").addClass("contactIconOnLoad");
+    }, 1500);
+
+    setTimeout(() => {
+        $(".hr3").addClass("hr4");
+    }, 1100 );
+
+    setTimeout(() => {
+        $(".bottomRow").addClass("bottomRowOnLoad");
+    }, 2500 );
 });
 
 
