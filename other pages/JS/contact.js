7579f7e80a6c0aff12d53f5668b9e351002e9614
DDumas551 <david.j.dumas@gmail.com>
2019-03-12 18:44:46 -0700

    Adds some contact page content

--- other pages/JS/contact.js
+++ other pages/JS/contact.js
@@ -2,12 +2,18 @@ $(document).ready(()=> {
     setTimeout(() => {
         $(".nameCol").addClass("nameColOnLoad");
     }, 400 );
-
-
-    
+    setTimeout(() => {
+        $(".hr1").addClass("hr2");
+    }, 500 );
+    setTimeout(() => {
+        $(".pdfImg").addClass("pdfImgOnLoad");
+    }, 1000 );
     setTimeout(() => {
         $(".bottomRow").addClass("bottomRowOnLoad");
-    }, 1000 );
+    }, 1250 );
+    setTimeout(() => {
+        $(".contactInvitation").addClass("contactInvitationOnLoad");
+    }, 2200);
 });
 
 
